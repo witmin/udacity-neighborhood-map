c2708acef8f537bcaa3df76a9585af0b6f8ed594
Millie Lin <linxinmin@gmail.com>
2017-12-01 18:54:29 +0800

    Pull locations array out into a const variable.

--- src/js/app.js
+++ src/js/app.js
@@ -1,49 +1,54 @@
 /**
- * @description define the model
+ * @description initial locations data
  */
 
+const locations = [
+    {
+        title: "Coliseum BART Station",
+        location: {
+            lat: 37.6916105,
+            lng: -122.4062711,
+        }
+    },
+    {
+        title: "Montgomery St. Station",
+        location: {
+            lat: 37.7894069,
+            lng: -122.40106730000002,
+        }
+    },
+    {
+        title: "Hayward Station",
+        location: {
+            lat: 37.66974459999999,
+            lng: -122.08703300000002
+        }
+    },
+    {
+        title: "San Francisco International Airport Station",
+        location: {
+            lat: 37.6159629,
+            lng: -122.3924154,
+        }
+    },
+    {
+        title: "Daly City BART Station",
+        location: {
+            lat: 37.7063632,
+            lng: -122.4692604,
+        }
+    }
+];
+
+/**
+ * @description define the app view model
+ */
 let AppViewModel = function () {
     let self = this;
 
     self.shouldShowNavigation = ko.observable(true);
 
-    self.locations = ko.observableArray([
-        {
-            title: "Coliseum BART Station",
-            location: {
-                lat: 37.6916105,
-                lng: -122.4062711,
-            }
-        },
-        {
-            title: "Montgomery St. Station",
-            location: {
-                lat: 37.7894069,
-                lng: -122.40106730000002,
-            }
-        },
-        {
-            title: "Hayward Station",
-            location: {
-                lat: 37.66974459999999,
-                lng: -122.08703300000002
-            }
-        },
-        {
-            title: "San Francisco International Airport Station",
-            location: {
-                lat: 37.6159629,
-                lng: -122.3924154,
-            }
-        },
-        {
-            title: "Daly City BART Station",
-            location: {
-                lat: 37.7063632,
-                lng: -122.4692604,
-            }
-        }
-    ]);
+    self.locations = ko.observableArray(locations);
 
     // Toggle navigation list display
     self.toggleNavigation = function () {
@@ -51,6 +56,7 @@ let AppViewModel = function () {
     }
 
 };
+
 
 function initMap() {
     let map = new google.maps.Map(document.getElementById('map'), {
