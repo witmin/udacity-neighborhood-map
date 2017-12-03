7dd4966eb7bfe58d3b9449e262951bd7283651f8
Millie Lin <linxinmin@gmail.com>
2017-12-03 11:35:57 +0800

    Transpile app.js to ES2015. Update README.

--- README.md
+++ README.md
@@ -1,29 +1,65 @@
 # Neighborhood Map
 
+# Get started
+If you want to develop the Neighborhood Map base on this version, please
+1. clone or download the repository to your local machine
+2. initialize git for version control
+3. if you have installed node.js on your machine, run `npm install` in terminal in the project root folder. You should have the dev-depenencies and preset installed. If you haven't install node.js, please go to [node.js official site](https://nodejs.org/en/) and get one.
+4. now your should be ready to start development
+  
 # Build project 
 - All sources files of javascript and css are located in folder `src`.
-- All build files are in folder `dist`
+- To update core javascript, please find `src/js/app.js`
+- You should run `npm run babel` to convert it to ES2015 if the index.html includes the `dist/js/app.js`.
+- You can also change the script src to `src/js/app.js` if you want to develop under ES6 version. 
+- All build output files are in folder `dist`
 
-## transpile javascript ES6 to ES2015 in Babel 
+## transpile javascript ES6 to ES2015 in Babel
+- ensure `<script src="dist/js/app.js"></script>` has been included before `</body`
+- open terminal to the root folder, run following command:  
 ```es62es2015
 npm run babel
-
 ```
+- The app.js in `/src/js` folder which is in ES6 should have been transpiled to ES2015. 
 
-# Render SCSS to CSS
-Transpile SCSS to CSS
+# Update CSS
+In this project, all CSS are written in .scss files and transpiled to CSS. 
+
+To modify CSS style, always find the source file in .scss in folder `./src/scss/`. 
+
+Once you make chages, you should run following command to transpile SCSS to CSS format. 
 
 ```scss2css
 npm run build-css
 ```
 
-Watch CSS: transpile SCSS to CSS when a change detected.
+To make CSS auto transpile the file when changes detected, you should try `watch` method to make it efficient.
+
+1. Install [SASS](http://sass-lang.com/install) to your local
+2. In terminal, `cd` to the project root folder, run following commend:
 ```scss2css
 npm run watch-css
 ```
+3. Done. Once you update styles in SCSS files, the SCSS file should be transpiled and sent to the output folder `dist/css/app.css`
+
+
+# Set yo Google Map with your API Key
+1. On the bottom of `index.html` before the `</body>`, you should find the google map JavaScript API script:
+```angular2html
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=Your_key">
+</script>
+```
+Please change `Your_key` to your api key. 
+If you need a Google Map API key, please go to the [Google Map Developer site](https://developers.google.com/maps/).  
+
 
 ## Dependencies
-* jQuery 3.2.1
-* normalize.scss v7.0.0 https://github.com/JohnAlbin/normalize-scss
-* knockout.js
-* Icon images are from feather icons https://feather.netlify.com/
+This project is using following frameworks and libraries
+* MVVM Framework - [knockout.js v3.4.2](http://knockoutjs.com/)
+* AJAX - [jQuery 3.2.1](https://api.jquery.com/jQuery.ajax/)
+* CSS preprocessor - [SASS](http://sass-lang.com/)
+* Initialise basic styles across browsers - [normalize.scss v7.0.0](https://github.com/JohnAlbin/normalize-scss) 
+* Map - [google map JavaScript API](https://developers.google.com/maps/documentation/javascript/)
+* Menu Icon image - [feather icons](https://feather.netlify.com/)
+* Wikipedia information - [MediaWiki API](https://www.mediawiki.org/wiki/API:Main_page)
--- dist/js/app.js
+++ dist/js/app.js
@@ -3,37 +3,185 @@
 /**
  * @description initial locations data
  */
-var locations = [{
+var initialPlaces = [{
     title: "Coliseum BART Station",
     location: {
         lat: 37.6916105,
         lng: -122.4062711
-    }
+    },
+    isActive: false
 }, {
     title: "Montgomery St. Station",
     location: {
         lat: 37.7894069,
         lng: -122.40106730000002
-    }
+    },
+    isActive: false
 }, {
     title: "Hayward Station",
     location: {
         lat: 37.66974459999999,
         lng: -122.08703300000002
-    }
+    },
+    isActive: false
 }, {
     title: "San Francisco International Airport Station",
     location: {
         lat: 37.6159629,
         lng: -122.3924154
-    }
+    },
+    isActive: false
 }, {
     title: "Daly City BART Station",
     location: {
         lat: 37.7063632,
         lng: -122.4692604
-    }
+    },
+    isActive: false
 }];
+
+var markers = [];
+var infowindow = void 0;
+/**
+ * @description set the Place object
+ * @param data
+ * @constructor
+ */
+var Place = function Place(data) {
+    this.title = ko.observable(data.title);
+    this.location = ko.observable(data.location);
+    this.lat = ko.observable(data.location.lat);
+    this.lng = ko.observable(data.location.lng);
+    this.isActive = ko.observable(data.isActive);
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.location.lat, data.location.lng),
+        title: data.title,
+        animation: google.maps.Animation.DROP
+    });
+
+    infowindow = new google.maps.InfoWindow({
+        maxWidth: 320
+    });
+
+    // Show marker on clicking
+    marker.addListener('click', function () {
+        populateInfoWindow(marker, infowindow);
+    });
+
+    markers.push(marker);
+};
+
+/**
+ * @description populate info window for a marker
+ * @param marker
+ * @param infowindow
+ */
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker !== marker) {
+        infowindow.marker = marker;
+        getWikiPage(marker.title);
+        infowindow.setContent("<h2 class=\"marker-title\">" + marker.title + "</h2><div id=\"info-content\"></div>\n<div id=\"response-container\"></div>");
+
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function () {
+            infowindow.marker = null;
+        });
+    }
+}
+
+/**
+ * @description add a marker to the markers array
+ * @param title {String}, location {object}
+ */
+function addMarker(title, location) {
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(location),
+        title: title,
+        animation: google.maps.Animation.DROP
+    });
+    markers.push(marker);
+}
+
+/**
+ * @description Hide markers from the map
+ */
+function clearMarkers() {
+    showAllMarkers(null);
+}
+
+/**
+ * @description Set all markers on the map
+ * @param map
+ */
+function showAllMarkers(map) {
+    var _loop = function _loop(i) {
+        markers[i].setMap(map);
+        markers[i].addListener('click', function () {
+            populateInfoWindow(markers[i], infowindow);
+        });
+    };
+
+    for (var i = 0; i < markers.length; i++) {
+        _loop(i);
+    }
+}
+
+/**
+ * @description delete markers and remove from makers array.
+ */
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+
+var responseContainer = document.querySelector('#response-container');
+
+/**
+ * @description fetch wikipedia API data for each place
+ * API doc: https://www.mediawiki.org/wiki/API:Main_page
+ */
+function getWikiPage(title) {
+    $.ajax({
+        url: '//en.wikipedia.org/w/api.php',
+        data: { action: 'query', list: 'search', srsearch: title, format: 'json' },
+        dataType: 'jsonp',
+        success: function success(data) {
+            // console.log(data);
+        }
+    }).done(populateWikiContent).fail(function (error) {
+        requestError(error);
+    });
+}
+
+/**
+ * @description get wiki content snippet and page url and show the page in info window
+ * @param data
+ */
+function populateWikiContent(data) {
+    var htmlContent = '';
+    var contentWrapper = document.querySelector("#info-content");
+    if (data) {
+        var snippet = data.query.search[0].snippet;
+
+        htmlContent = "<p>Relevant entry snippet on Wikipedia:</p><p class=\"snippet\">" + snippet + "</p>";
+    } else {
+        htmlContent = '<div class="error-no-content">No wikipedia content available</div>';
+    }
+
+    contentWrapper.insertAdjacentHTML('beforeEnd', htmlContent);
+}
+
+/**
+ * @description show request error message
+ * @param e
+ */
+function requestError(e) {
+    console.log(e);
+    responseContainer.insertAdjacentHTML('beforeEnd', "<p class=\"network-warning error\"> There was an error to make the request.</p>");
+}
 
 /**
  * @description define the app view model
@@ -43,54 +191,138 @@ var AppViewModel = function AppViewModel() {
 
     self.shouldShowNavigation = ko.observable(true);
 
-    // Map data
-    self.locations = ko.observableArray(locations);
-
     /**
      * @description toggle navigation display
      */
     self.toggleNavigation = function () {
         self.shouldShowNavigation(!self.shouldShowNavigation());
     };
+    /**
+     * @description add places to observableArray to init tha app
+     */
+    self.places = ko.observableArray([]);
+
+    initialPlaces.forEach(function (place) {
+        self.places.push(new Place(place));
+    });
 
     /**
-     * @description define the map data value for BART
+     * @description Initialise the map
      * @type {{zoom: number, lat: number, lng: number}}
      */
-    self.bartMap = { zoom: 11, lat: 37.8271784, lng: -122.2913078 };
-};
+    self.mapOptions = { zoom: 8, lat: 37.71, lng: -122.2913078 };
 
-/**
- * @description create custom bindings for google map
- */
-ko.bindingHandlers.map = {
-    init: function init(element, valueAccessor, allBindings) {
-        // get the latest data for the map
-        var value = valueAccessor();
-        var valueUnwrapped = ko.unwrap(value);
+    self.options = {
+        zoom: 11,
+        center: new google.maps.LatLng(37.71, -122.2913078),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-        var zoom = allBindings.get('mapZoom') || 11;
-        var lat = allBindings.get('mapCenterLat') || 37.8271784;
-        var lng = allBindings.get('mapCenterLng') || -122.2913078;
+    self.mapElement = document.getElementById('map');
+    self.map = new google.maps.Map(self.mapElement, self.options);
 
-        var options = {
-            zoom: zoom,
-            center: new google.maps.LatLng(lat, lng),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+    showAllMarkers(self.map);
 
-        // If map data is undefined, load the default value
-        if (valueUnwrapped === undefined) {
-            var map = new google.maps.Map(element, options);
-        } else {
-            // If map value is defined, load the latest data value in model
-            options = {
-                zoom: value.zoom,
-                center: new google.maps.LatLng(value.lat, value.lng)
-            };
-            var _map = new google.maps.Map(element, options);
+    /**
+     * @description toggle places active status
+     * @param clickedPlace
+     */
+    self.setActivePlace = function (clickedPlace, event) {
+        for (var i = 0, len = self.places().length; i < len; ++i) {
+            var place = self.places()[i];
+            place.isActive(false);
         }
-    }
+
+        clickedPlace.isActive(true);
+        var context = ko.contextFor(event.target);
+
+        // get the clicked place marker data
+        var marker = markers[context.$index()];
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        // Make the marker bounce once
+        setTimeout(function () {
+            marker.setAnimation(null);
+        }, 1400);
+
+        // show marker info window on clicked place
+        populateInfoWindow(marker, infowindow);
+    };
+
+    /**
+     * @description reset places with initial data
+     */
+    self.initPlaces = function () {
+        self.resetPlacesStatus();
+        self.places.removeAll();
+        initialPlaces.forEach(function (place) {
+            self.places.push(new Place(place));
+        });
+    };
+    /**
+     * @description reset all places status to inactive
+     */
+    self.resetPlacesStatus = function () {
+        for (var i = 0, len = self.places().length; i < len; ++i) {
+            var place = self.places()[i];
+            place.isActive(false);
+        }
+    };
+
+    /**
+     * @description delete places data from the array
+     */
+    self.deletePlaces = function () {
+        self.places.removeAll();
+    };
+
+    /**
+     * @description filter places with the keyword input
+     */
+    self.keyword = ko.observable("");
+
+    self.filterPlaces = function () {
+        deleteMarkers();
+        // Reset the list and markers if keyword is empty
+        if (self.keyword() === "") {
+            self.initPlaces();
+        } else {
+            var filterResults = [];
+            // compare string and update the list
+            for (var i = 0, len = self.places().length; i < len; ++i) {
+                var title = self.places()[i].title().toString();
+                var placeTitle = title.toLowerCase();
+                var keyword = self.keyword().toString().toLowerCase();
+
+                // If the item includes the keyword, add the place to results array and add marker
+                if (placeTitle.includes(keyword)) {
+                    filterResults.push(self.places()[i]);
+                    addMarker(title, self.places()[i].location());
+                }
+            }
+            // Reset the self.places data with the filtered results
+            self.deletePlaces();
+            filterResults.forEach(function (place) {
+                self.places.push(place);
+                console.log(place);
+            });
+        }
+        // Show all markers with the filtered results
+        showAllMarkers(self.map);
+    };
+
+    /**
+     * @description reset filter
+     */
+    self.resetFilter = function () {
+        // delete markers
+        deleteMarkers();
+        // reset keyword
+        self.keyword("");
+        // Init Places
+        self.initPlaces();
+        // reset markers
+        showAllMarkers(self.map);
+    };
 };
 
 $(function () {
